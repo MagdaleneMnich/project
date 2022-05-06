c7bbe26c1a737e5ea23cb49a3a7409374c926a9d
Magda <s0574860@htw-berlin.de>
2022-05-06 14:56:01 +0200

    beginning of the project

---
+++ test.py
@@ -0,0 +1,23 @@
+from dbm.ndbm import library
+
+
+library(dplyr)
+
+library(ggplot2)
+
+library(tidyr)
+
+library(plotly)
+
+library(purrr)
+
+library(treemapify)
+
+library(reshape2)
+
+library(gganimate)
+
+library(ggridges)
+df <- read.csv("tv_shows.csv")
+
+head(df)
